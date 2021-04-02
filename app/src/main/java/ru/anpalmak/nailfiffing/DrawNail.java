a002f6966d113350601a238b785f45b4af180967
anpalmak1 <73543260+anpalmak1@users.noreply.github.com>
2021-04-02 14:20:47 +0400

    a

--- app/src/main/java/ru/anpalmak/nailfiffing/DrawNail.java
+++ app/src/main/java/ru/anpalmak/nailfiffing/DrawNail.java
@@ -1,31 +1,49 @@
 package ru.anpalmak.nailfiffing;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.util.AttributeSet;
 import android.view.View;
 
 public class DrawNail extends View {
-float bottom; float right; float left; float top;
-    public DrawNail(Context context, float bottom, float right, float left, float top) {
-        super(context);
+float bottom; float right; float left; float top; float height; float widht;
+    public DrawNail(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        invalidate();
+
+    }
+    public void SetNail(float bottom, float right, float left, float top, float height, float wight) {
         this.bottom=bottom;
         this.right=right;
         this.left=left;
         this.top=top;
+        this.height=height;
+        this.widht=wight;
     }
-
     @Override
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+       Bitmap nail= BitmapFactory.decodeResource(getResources(), R.drawable.background);
         Paint paint = new Paint();
-        // Выбираем кисть
         paint.setStyle(Paint.Style.FILL);
         paint.setColor(Color.RED);
-        canvas.drawRect(left, top, right, bottom, paint);
-
+      /*  canvas.drawBitmap(nail, widht/2-nail.getWidth()/2, height-nail.getHeight(), paint);
+      canvas.drawColor(0x00AAAAAA);
+left =100; right=200; top=100; bottom=200;
+        */
+         canvas.drawRect(100, 100, 200, 200, paint);
+        RectF rect = new RectF(left, top, right, bottom);
+      //  canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.balelerina), null, rect, null);
+     //  drawing(canvas);*/
 
     }
+
 
 }
--- app/src/main/java/ru/anpalmak/nailfiffing/Draw_design_avtivity.java
+++ app/src/main/java/ru/anpalmak/nailfiffing/Draw_design_avtivity.java
@@ -2,18 +2,23 @@ package ru.anpalmak.nailfiffing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Display;
+import android.view.View;
 import android.widget.ImageView;
 
 import static android.graphics.Color.RED;
 
 public class Draw_design_avtivity extends AppCompatActivity {
     private float bottom, left, right, top;
-public Canvas canvas;
+    public ImageView nail; View n;
+
+public int height, width;
    /* Display display = getWindowManager().getDefaultDisplay();
     int width = display.getWidth();  // deprecated
     int height = display.getHeight();  // deprecated*/
@@ -21,18 +26,37 @@ public Canvas canvas;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw_design_avtivity);
-        ImageView nail=findViewById(R.id.imageView5);
+        nail=findViewById(R.id.imageView5);
         DisplayMetrics displaymetrics = getResources(). getDisplayMetrics();
-        int height =displaymetrics.heightPixels;
-        int width =displaymetrics.widthPixels;
+        height =displaymetrics.heightPixels;
+        width =displaymetrics.widthPixels;
+        n=findViewById(R.id.view2);
 
-        bottom=height-nail.getMaxHeight()/3*2;
-        top=bottom+nail.getMaxHeight()/2;
-        left=width/2+nail.getMaxWidth()/3*2;
-        right=width/2-nail.getMaxWidth()/3*2;
-        new DrawNail(getApplicationContext(), bottom, right, left, top);
-        //DrawNail.onDraw(canvas);
 
     }
+    public void onWindowFocusChanged(boolean hasFocus) {
+        // TODO Auto-generated method stub
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
 
+            int fingerHeight = nail.getHeight();
+            int fingerWidth = nail.getWidth();
+
+            bottom = height - fingerHeight / 3 * 2;
+            top = bottom - fingerHeight / 2;
+            left = width / 2 - fingerWidth / 3 ;
+            right = width / 2 + fingerWidth / 3 ;
+
+
+            // rgbFrameBitmap.setPixels(getRgbBytes(), 0, width, 0, 0, width, height);
+          //  setContentView(new DrawNail(this, bottom, right, left, top, height, width));
+
+            //   canvas.drawBitmap(rgbFrameBitmap, frameToCropTransform, null)
+            // DrawNail.invalidate(bottom, right, left, top);
+
+
+        }
+
+
+    }
 }
